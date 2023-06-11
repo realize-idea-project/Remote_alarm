3950050a198842f1684d202d332e561dcbfc3ca0
devsince2021 <dev.ryu2021@gmail.com>
2023-06-12 00:51:53 +0900

    제목 추가

--- src/agree/Agree.tsx
+++ src/agree/Agree.tsx
@@ -10,12 +10,15 @@ const Agree: FC<Props> = ({ onAgree }) => {
     <div className="board">
       <div className="content">
         <div className="content_text">
+          <div style={{ marginBottom: "2rem", fontWeight: 700 }}>
+            고기차맨 키카
+          </div>
           <div>무인 키카에 온 것 환영~ </div>
           <div>지금부터 2시간 이용 가능하시고</div>
           <div>이용중 발생한 문제는 손님책임 동의?</div>
         </div>
         <button className="button_ok" onClick={onAgree}>
-          ok염
+          ok염 ㄹㄱ
         </button>
       </div>
     </div>
