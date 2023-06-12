4fd9706dd4dab354cfa6c4f18d970021bd9a5d7b
devsince2021 <dev.ryu2021@gmail.com>
2023-06-13 00:33:00 +0900

    자동재생 관련 이슈 기록

--- src/audio/AudioTag.tsx
+++ src/audio/AudioTag.tsx
@@ -7,6 +7,10 @@ interface Props {
   path: string;
 }
 
+// 자동재생 안되는 이슈
+// 최초에 유저가 실행하지 않으면 구글의 정책으로 인해 audio 태그에서 오류가 남
+// 그래서 음소거 상태로 최초에 1번 실행
+// 크롬 설정을 변경하면 그냥 자동재생이 가능해짐
 export const AudioTag = forwardRef<CustomAudioRef, Props>(({ path }, ref) => {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isMuted, setIsMuted] = useState(true);
