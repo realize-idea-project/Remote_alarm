9a1c694705c3c2502f395a5e576527e12f27723d
devsince2021 <dev.ryu2021@gmail.com>
2023-06-13 01:11:33 +0900

    시계 폰트 사이즈 수정

--- src/countdown/clock/Clock.tsx
+++ src/countdown/clock/Clock.tsx
@@ -2,6 +2,7 @@
 import { FC, useEffect } from "react";
 import { useTime } from "react-timer-hook";
 import { AlarmSchedule } from "../types";
+import { styled } from "styled-components";
 
 const getFormattedMinutes = (minute: number) => {
   const stringified = minute.toString();
@@ -15,26 +16,31 @@ interface Props {
 export const Clock: FC<Props> = ({ onChangeSecond }) => {
   const { seconds, minutes, hours, ampm } = useTime({ format: "12-hour" });
 
+  const formattedHour = hours === 0 ? `12` : `${hours}`;
+
+  const formattedMinutes = getFormattedMinutes(minutes);
   const formattedSecond =
     seconds.toString().length === 1 ? `0${seconds}` : seconds;
 
-  const formattedHour = hours === 0 ? `12` : hours;
-
   useEffect(() => {
-    onChangeSecond([
-      formattedHour.toString(),
-      getFormattedMinutes(minutes),
-      ampm,
-    ]);
+    onChangeSecond([formattedHour.toString(), formattedMinutes, ampm]);
   }, [seconds]);
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <div style={{ fontSize: "100px" }}>
-        <span>{formattedHour}</span>:<span>{minutes}</span>:
+    <Container>
+      <Numbers>
+        <span>{formattedHour}</span>:<span>{formattedMinutes}</span>:
         <span>{formattedSecond}</span>
         <span>{ampm}</span>
-      </div>
-    </div>
+      </Numbers>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  text-align: "center";
+`;
+
+const Numbers = styled.div`
+  font-size: 4rem;
+`;
